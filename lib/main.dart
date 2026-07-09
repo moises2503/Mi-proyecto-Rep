290061c56e3415ed3539a2ad8c139773f1084cd0
moises2503 <moisesjuarezgonzalez7@gmail.com>
2026-07-09 16:18:32 -0600

    Cambio
    Se Modifico el nombre

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Music Player',
+      title: 'HOLA MUNDO',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.black,
       ),
